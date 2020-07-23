b496871b7daac626235f64a1414657426252beba
KristinaStepanova <kri18stina@gmail.com>
2020-07-23 16:27:11 +0300

    main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxSpinnerModule } from "ngx-spinner";
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { PostService } from './services/post.service';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostItemComponent } from './components/post-item/post-item.component';
+import { ManagePostFormComponent } from './components/manage-post-form/manage-post-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ShoppingListComponent,
+    PostsComponent,
+    PostItemComponent,
+    ManagePostFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule,
+    NgxSpinnerModule 
   ],
-  providers: [],
+  providers: [
+    PostService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
